7a8b2b04ac0dfa45bfe8f77ea06fb46eef108183
Hannes Jasper Maas <jaspermaas@Hanness-Mac-mini.local>
2025-11-20 23:28:08 +0100

    update

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,5 +1,30 @@
 #! /bin/bash
 
+# Apple Global Domain
+echo "Configuring Apple Global Domain..."
+defaults write NSGlobalDomain AppleIconAppearanceTheme -string "RegularDark"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Apple MultitouchMouse
+echo "Configuring Apple MultitouchMouse..."
+defaults write com.apple.MultitouchMouse MouseButtonMode -string "TwoButton"
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string "TwoButton"
+
+# Apple Display Settings
+echo "Configuring Apple Display Settings..."
+defaults write com.apple.Display-Settings.extension showListByDefault -bool true
+
+# Apple Spotlight
+echo "Configuring Apple Spotlight..."
+defaults write com.apple.Spotlight PasteboardHistoryEnabled -bool true
+
+# Apple Window Manager
+echo "Configuring Apple Window Manager..."
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
+
 # Apple Dock
 echo "Configuring Apple Dock..."
 defaults write com.apple.dock autohide -bool true
